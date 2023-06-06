66fee9cbc3471085f3237745024dd067c7b31b05
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-06 22:35:28 +0800

    create two function: function that compacts the expression, and an array to solve the expression

--- script/main.js
+++ script/main.js
@@ -27,7 +27,7 @@
             operatorFlag = true
             console.log(operatorFlag)
             display()
-        } else if(['add','subtract','multiply','divide','modulo','equal'].includes(e.target.id) && operatorFlag && expression.length != 0){ // to prevent be
+        } else if(['add','subtract','multiply','divide','modulo'].includes(e.target.id) && operatorFlag && expression.length != 0){ // to prevent be
             //if equals solve
             console.log(e.target.id)
             expression.push(e.target.textContent)
@@ -35,28 +35,79 @@
             operatorFlag = false
             console.log(operatorFlag)
             display()
+        } else if(e.target.id == 'equal' && operatorFlag && expression.length != 0){
+            console.log(e.target.id)
+            //dont append
+            expression.push(e.target.textContent)
+
+            operatorFlag = false
+            console.log(operatorFlag)
+            
+            let result = solve()
+            //removing equals in the display array because it was included first to set as flag for the compact function - this is lesser code
+            expression.pop() 
+            //temporary to display only
+            expression.push(`=${result}`)
+
+            display()
+            //!!!!!!
+            //clear array
+            //add only the latest result as initial valueS
+            //put the old as history
         }
-        d()
+        
     }
 
-    function d(){
-        //put numbers in a s
+    function solve(){
+        let compactExpression = compact()
+        if(compactExpression.length == 1) {
+            return compactExpression[0]
+        }
+        let accumulator = 0
+
+        //!!!!!!
+        //set to handle pemdas
+
+        //solves the expression left to right
+        //every loop, the first 3 values from the array are evaluated separately from the rest
+        //then removed from the array, and the accumulator is added
+        for(let i = compactExpression.length; i > 1; i = i - 2){
+            console.log(`${i} = ${compactExpression}`)
+            let operand1 = +compactExpression[0]
+            let operator = compactExpression[1] //consecutive
+            let operand2 = +compactExpression[2] //2nd consective
+            console.log(`operand 1 = ${operand1}, operator 2 = ${operator}, operand 3 = ${operand2}`)
+            accumulator = operate(operand1, operand2, operator)
+            compactExpression=compactExpression.slice(3)
+            compactExpression.splice(0, 0, accumulator)
+        }
+
+        return accumulator
+    }
+
+    function compact(){
+        //compacts the original array to a managaeable array
         let tempExpression = []
         let tempValue = ''
-        //loop through array
+        //combines consecutive numbers till an operator is met
         expression.forEach((value) => {
-            if(['+', '-', '*', '/', '%', '='].includes(value)){
-                tempExpression.push(tempValue)
-                tempValue = ''
-                tempExpression.push(value)
-            } else {
-                tempValue += value
-            }
+                if(['+', '-', '*', '/', '%'].includes(value)){
+                    // operator is included being push
+                    tempExpression.push(tempValue)
+                    tempValue = ''
+                    tempExpression.push(value)
+                } else if (value == '='){
+                    // the = operator is not included being push
+                    tempExpression.push(tempValue)
+                    tempValue = ''
+                } else {
+                    // concatenates number
+                    tempValue += value
+                }
             }
         )
-        console.log(tempExpression)
-            //if number - concatenate all
-            //if encounter - operator - push number on new list as well the new operator    
+        console.log(`Compacted = ${tempExpression}`)
+        return tempExpression
     }
 
 
@@ -65,7 +116,7 @@
     }
 
     function display(){
-        //concatenate them
+        //to dislay current values in the panel
         let tempExpression = toString()
         document.querySelector('#panel').textContent = tempExpression
     }
