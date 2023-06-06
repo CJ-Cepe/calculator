3aeb69d633e69b11223212bafeba2f6ddfd4b7e7
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-06-06 18:16:22 +0800

    create function to clear, backspace, and display operator and operands

--- script/main.js
+++ script/main.js
@@ -1,22 +1,98 @@
-//set rule
-//for arithmetic function, use proper(specific) term
-function addition(firstAddend, secondAddend){
-    return firstAddend + secondAddend
-}
 
-function subtraction(minued, subtrahend){
-    return minued - subtrahend
-}
 
-function multiplication(multiplicand, multiplier){
-    return multiplicand * multiplier
-}
+//self invoked function
+(function(){
+    
+    mainContainer = document.querySelector('main')
+    let expression = []
+    let operatorFlag = true
+    
+    mainContainer.onclick = function (e){
+        if(e.target.id === 'clear'){
+            //clear 
+            console.log(e.target.id)
+            expression = []
+            display()
+        } else if(e.target.id === 'backspace'){
+            //pop
+            console.log(e.target.id)
+            expression.pop()
+            display()
+        } else if(e.target.id === 'panel'){
+            //do nothing nothing
+        } else if(['zero','one','two','three','four','five','six','seven','eight','nine'].includes(e.target.id)){
+            console.log(e.target.id)
+            expression.push(e.target.textContent)
+            
+            operatorFlag = true
+            console.log(operatorFlag)
+            display()
+        } else if(['add','subtract','multiply','divide','modulo','equal'].includes(e.target.id) && operatorFlag && expression.length != 0){
+            //if equals solve
+            console.log(e.target.id)
+            expression.push(e.target.textContent)
 
-function division(dividend, divisor){
-    return dividend / divisor
-}
+            operatorFlag = false
+            console.log(operatorFlag)
+            display()
+        }
+    }
 
-function modulo(dividend, divisor){
-    return dividend % divisor
-}
+    function toString(){
+        return expression.join('')
+    }
+
+    function display(){
+        //concatenate them
+        let tempExpression = toString()
+        document.querySelector('#panel').textContent = tempExpression
+    }
+
+    function addition(firstAddend, secondAddend){
+        return firstAddend + secondAddend
+    }
+    
+    function subtraction(minued, subtrahend){
+        return minued - subtrahend
+    }
+    
+    function multiplication(multiplicand, multiplier){
+        return multiplicand * multiplier
+    }
+    
+    function division(dividend, divisor){
+        return dividend / divisor
+    }
+    
+    function modulo(dividend, divisor){
+        return dividend % divisor
+    }
+    
+    function operate(operand1, operand2, operator){
+        switch(operator){
+            case `+` :
+                return addition(operand1, operand2)
+            case `-` :
+                return subtraction(operand1, operand2)
+            case `*` :
+                return multiplication(operand1, operand2)
+            case `/` :
+                return division(operand1, operand2)
+            case `%` :
+                return modulo(operand1, operand2)
+            default :
+                console.log('invalid operator')
+        }
+    }
+
+
+
+
+
+})(); //self invoking function
+
+
+
+
+
 
